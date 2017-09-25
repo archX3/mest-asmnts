38d3cfd4b35394832f8b91649240758816cb63b6
Kobina Koomson <georgeranch31@gmail.com>
2017-09-25 16:19:18 +0000

    added send me an email

---
+++ sendMeAnEmail.py
@@ -0,0 +1,10 @@
+import smtplib
+import smtpd
+
+guests = {1 : {"name" : "Andrew PHP Berkowitz", "email" : "andrew@meltwater.org"},
+          2 : {"name" : "Oladele Ibrahim", "email" : "oladele.ibrahim@meltwater.org"},
+          3 : {"name" : "Francis", "Addai" : "francis.addai@meltwater.org"},
+          4 : {"name" : "Michale Prince", "email" : "michael.prince@meltwater.org"}
+          }
+
+
